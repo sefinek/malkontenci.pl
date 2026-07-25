65cd4ea7726c164ced2436f1c63d382e36755cae
Sefinek <contact@sefinek.net>
2026-07-25 13:02:39 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-	{ ignores: ['node_modules', '**/*.min.js', '**/*bundle*', 'build/**', 'dist/**'] },
 	js.configs.recommended,
 	{
 		languageOptions: {
