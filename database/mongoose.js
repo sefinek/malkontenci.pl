ab13abcb65b8f30b6f1fa9c18aac1c2a8e6f71ce
Sefinek <contact@sefinek.net>
2026-08-23 04:04:09 +0200

    mongoose  ^9.9.2  →  ^9.9.3

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,17 +1,9 @@
 const { connect, connection } = require('mongoose');
 
-connect(process.env.MONGODB_URL).catch(err => {
-	console.error('Failed to connect to the database', err);
-	process.exit(1);
-});
+connect(process.env.MONGODB_URL).catch(err => console.error('Failed to connect to the database', err));
 
 connection.on('connected', () => console.log('Successfully connected to the database'));
-
 connection.on('disconnected', () => console.warn('MongoDB disconnected!'));
-
-connection.on('error', err => {
-	console.error('MongoDB error:', err.message);
-	process.exit(1);
-});
+connection.on('error', err => console.error('MongoDB error:', err.message));
 
 module.exports = connection;
